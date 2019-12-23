d18748e52bea4fca12308797d6a44549ce163306
kawaaa26 <unic.almj@yahoo.com>
2019-12-23 19:40:53 +0900

    :zap: view control for renderer process (<webview>).

--- js/mainwindow.js
+++ js/mainwindow.js
@@ -1,8 +1,26 @@
 // editor initialization
+// commonJS module. may converted to ES6 for current version. just ignore for the time
+// importing needed libr// for those requiresries for those requires
 require('ace-min-noconflict');
 require('ace-min-noconflict/mode-markdown');
 
+// "ace" perhaps is enabled by above requires.
 var editor = ace.edit("editor");
+
+// set Markdown for sytax highlight, suggestions, and auto-completion here
 editor.getSession().setMode("ace/mode/markdown");
+
+// fold lines for longer lines.
 editor.getSession().setUseWrapMode(true);
+
+// give a focus on the editor.
 editor.focus();
+
+// open developer tools when DEBUG_GUEST=true.
+var webveiw = document.getElementById('webview');
+
+if (process.env.DEBUG_GUEST) {
+  webview.addEventListener('dom-ready', () => {
+    webview.openDevTools()
+  });
+}
---
+++ js/webview.js
@@ -0,0 +1,2 @@
+// show current preload status on console
+console.log("js/webview.js was loaded");
